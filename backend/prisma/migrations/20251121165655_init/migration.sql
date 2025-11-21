bcabe4e4d1b237ec8437a8dce1fb41dade4355a0
Jitsben Andree <Jitsben17@gmail.com>
2025-11-21 12:24:09 -0500

    l

---
+++ backend/prisma/migrations/20251121165655_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `categoria` on the `Gasto` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Gasto" DROP COLUMN "categoria";
